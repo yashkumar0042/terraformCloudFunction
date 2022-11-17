b88426c11b346f4bbc54df7ef64fd46f88a96ee2
Yaswant Singh <53463349+yashkumar0042@users.noreply.github.com>
2022-11-17 21:13:58 +0100

    Update main.py

--- cloudFuntion/main.py
+++ cloudFuntion/main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello Python!'
+        return f'Hello Python3!'
