885caed81112733e71a730fda933724538c3e699
Yaswant Singh <53463349+yashkumar0042@users.noreply.github.com>
2022-11-17 19:18:14 +0100

    Create main.tf

---
+++ tfScripts/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.82.0"
+    }
+  }
+ backend "gcs" {
+   bucket  = "test-bucket5878"
+   prefix  = "terraform/state"
+ }
+}
+provider "google" {
+  project = "${var.project_id}"
+  region  = "us-central1"
+}
+
+data "archive_file" "dotfiles" {
+  type        = "zip"
+  output_path = "${path.module}/files/dotfiles.zip"
+  excludes    = [ "${path.module}/unwanted.zip" ]
+  source_dir = "../"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket5878"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./path/to/zip/file/which/contains/code"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
