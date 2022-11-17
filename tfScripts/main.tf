5cd0e45aa9b63acde4970b77e312b5c1dc8ad310
Yaswant Singh <53463349+yashkumar0042@users.noreply.github.com>
2022-11-17 21:44:12 +0100

    Update main.tf

--- tfScripts/main.tf
+++ tfScripts/main.tf
@@ -60,3 +60,7 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
 }
+
+output "version" {
+      value       = google_cloudfunctions_function.function.versions
+}
