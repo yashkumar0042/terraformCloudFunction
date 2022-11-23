2c14f4ec3aac24f84f1c96ecb7aa5094ba5b6e5f
Yaswant Singh <53463349+yashkumar0042@users.noreply.github.com>
2022-11-23 14:39:27 +0100

    Update main.tf

--- tfScripts/main.tf
+++ tfScripts/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 #This block will generate the zip file name with any random value
 locals {
-  cf_zip_archive_name = "cf-v${var.v_id}-${data.archive_file.zipfiles.output_sha}.zip"
+  cf_zip_archive_name = "cf-v${var.build_version}-${data.archive_file.zipfiles.output_sha}.zip"
   
   
 }
@@ -60,9 +60,9 @@ resource "google_cloudfunctions_function" "function" {
 }
 resource "google_firestore_document" "fireVersionDoc" {
   project = "${var.project_id}"
-  collection  = "${var.v_id}"
+  collection  = "v${var.build_version}"
   document_id = "cloudVersion-document"
-  fields      = "{\"CFVersion\":{\"mapValue\":{\"fields\":{\"CloudBuildVersion\":{\"stringValue\":\"${var.v_id}\"}}}}}"
+  fields      = "{\"CFVersion\":{\"mapValue\":{\"fields\":{\"CloudBuildVersion\":{\"stringValue\":\"v${var.build_version}\"}}}}}"
 }
 # IAM entry for all users to invoke the function
 resource "google_cloudfunctions_function_iam_member" "invoker" {
