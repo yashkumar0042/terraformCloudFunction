4c1bed8aec7a91cc8ce9ee1645811132f2377e2e
Yaswant Singh <53463349+yashkumar0042@users.noreply.github.com>
2022-11-23 11:24:20 +0100

    Update main.tf

--- tfScripts/main.tf
+++ tfScripts/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 #This block will generate the zip file name with any random value
 locals {
-  cf_zip_archive_name = "cf-v${var.version_id}-${data.archive_file.zipfiles.output_sha}.zip"
+  cf_zip_archive_name = "cf-v${var.v_id}-${data.archive_file.zipfiles.output_sha}.zip"
   
   
 }
